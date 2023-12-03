8e5441bc6c4836be9d3c92b66051f171577fd005
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-12-03 16:14:27 +0100

    Add solution

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,17 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+enum Weekday {
+  MONDAY = 1,
+  TUESDAY = 1,
+  WEDNESDAY = 1,
+  THURSDAY = 1,
+  FRIDAY = 1,
+  SATURDAY = 0,
+  SUNDAY = 0,
+}
+
+const isWeekend = (day: Weekday): boolean => {
+//   якщо робочий день - повертає true, а вихідний - повертає false;
+  return Boolean(day === 1);
+}
--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,7 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare (top:AllType, bottom:AllType): AllType {
   return {
     name: top.name,
     color: top.color,
