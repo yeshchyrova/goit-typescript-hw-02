3c01afd8e3456ae39fe98bf1db46014cf1753be5
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-12-04 22:07:01 +0100

    Add generics part

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,9 +1,12 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface Props {
+  title: string;
+}
 
 class Component {
-  constructor (public props:T) {
+  constructor (public props:Props) {
 
   }
 }
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,9 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<T> {
+  key: T;
+  value: T;
 }
 
 export {};
